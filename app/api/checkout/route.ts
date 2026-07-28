d4c6d00d289519f47a21c5dc6588057a87e11341
kostas <kostasxaldoupis@gmail.com>
2026-07-28 13:25:25 +0300

    add paypal payment, not important

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: Request) {
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-      payment_method_types: ["card"],
+      payment_method_types: ["card","paypal"],
       line_items,
       success_url: `${process.env.NEXT_PUBLIC_APP_URL}/success`,
       cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/cancel`,
